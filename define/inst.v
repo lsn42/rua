4f9229bd343f06f97e8e7ddb1c2dfd31cd2eb85d
lsn42 <24622966+lsn42@users.noreply.github.com>
2022-03-27 02:36:19 +0800

    adopt another design method. modify id, let it take registers value rather than ex. id now preprocessing registers value and immediates, then send them as operands to ex.

--- define/inst.v
+++ define/inst.v
@@ -1,14 +1,12 @@
 // R type instruction, used for computation, total count: 9
 // R型指令，用于计算，共9条
 `define INST_OP_TYPE_R 7'b0110011
-`define INST_FUNCT3_ADD 3'b000
-`define INST_FUNCT3_SUB 3'b000
+`define INST_FUNCT3_ADD_SUB 3'b000
 `define INST_FUNCT3_SLL 3'b001
 `define INST_FUNCT3_SLT 3'b010
 `define INST_FUNCT3_SLTU 3'b011
 `define INST_FUNCT3_XOR 3'b100
-`define INST_FUNCT3_SRL 3'b101
-`define INST_FUNCT3_SRA 3'b101
+`define INST_FUNCT3_SRL_SRA 3'b101
 `define INST_FUNCT3_OR 3'b110
 `define INST_FUNCT3_AND 3'b111
 
@@ -38,8 +36,7 @@
 `define INST_FUNCT3_ORI  3'b110
 `define INST_FUNCT3_ANDI 3'b111
 `define INST_FUNCT3_SLLI 3'b001
-`define INST_FUNCT3_SRLI 3'b101
-`define INST_FUNCT3_SRAI 3'b101
+`define INST_FUNCT3_SRLI_SRAI 3'b101
 
 // system call
 `define INST_OP_TYPE_I_S 7'b1110111
--- rtl/id.v
+++ rtl/id.v
@@ -2,67 +2,84 @@
 `include "define/inst.v"
 
 module id(
-    input wire clk, input wire rst,
-    input wire[`XLEN_WIDTH] inst, input wire[`XLEN_WIDTH]inst_addr,
+    input wire[`XLEN_WIDTH] inst, input wire[`XLEN_WIDTH] inst_addr,
 
     output reg[`REG_ADDR] regs_addr1, output reg[`REG_ADDR] regs_addr2,
-    output reg regs_write_en,
-    output reg[`REG_ADDR] regs_write_addr,
+    input wire[`XLEN_WIDTH] regs_data1, input wire[`XLEN_WIDTH] regs_data2,
 
-    output reg mem_read_en, output reg mem_write_en
+    output reg[`XLEN_WIDTH] operand1, output reg[`XLEN_WIDTH] operand2,
+
+    output reg pause
   );
 
+  // instruction identification
   wire[6: 0] opcode = inst[6: 0];
   wire[2: 0] funct3 = inst[14: 12];
   wire[6: 0] funct7 = inst[31: 25];
 
-  wire[`REG_ADDR] rd = inst[11: 7];
+  // the address of register source and destination
   wire[`REG_ADDR] rs1 = inst[19: 15];
   wire[`REG_ADDR] rs2 = inst[24: 20];
+  wire[`REG_ADDR] rd = inst[11: 7];
+
+  // seperate different kinds of immediate from instruction
+  wire[11: 0] imm_i = inst[31: 20];
+  wire[11: 0] imm_s = {inst[31: 25], inst[11: 7]};
+  wire[19: 0] imm_u = inst[31: 12];
+  wire[11: 0] imm_b = {inst[31], inst[7], inst[30: 25], inst[11: 8]};
+  wire[19: 0] imm_j = {inst[31], inst[19: 12], inst[20], inst[30: 21]};
 
   // combinational logic
   // 组合逻辑
   always @(* ) begin
-    regs_addr1 <= 0;
-    regs_addr2 <= 0;
-    regs_write_en <= `false;
-    regs_write_addr <= 0;
+    regs_addr1 = 0;
+    regs_addr2 = 0;
+    operand1 = 0;
+    operand2 = 0;
+    pause = `false;
     case (opcode)
       `INST_OP_TYPE_R: begin
-        regs_addr1 <= rs1;
-        regs_addr2 <= rs2;
-        regs_write_en <= `true;
-        regs_write_addr <= rd;
+        regs_addr1 = rs1;
+        regs_addr2 = rs2;
+        operand1 = regs_data1;
+        operand2 = regs_data2;
       end
-      `INST_OP_TYPE_I_JALR,
+      `INST_OP_TYPE_I_JALR: begin
+        regs_addr1 = rs1;
+        operand1 = regs_data1;
+        operand2 = $signed(imm_i);
+        // TODO: pause
+      end
       `INST_OP_TYPE_I_I,
-      `INST_OP_TYPE_I_S: begin
-        regs_addr1 <= rs1;
-        regs_write_en <= `true;
-        regs_write_addr <= rd;
+      `INST_OP_TYPE_I_L: begin
+        regs_addr1 = rs1;
+        operand1 = regs_data1;
+        // SLLI/SRLI/SRAL won't affect by sign extension
+        operand2 = $signed(imm_i);
       end
-      `INST_OP_TYPE_I_L:begin
-        regs_addr1 <= rs1;
-        regs_write_en <= `true;
-        regs_write_addr <= rd;
-        mem_read_en <= `true;
-      end
+      `INST_OP_TYPE_I_S:
+        ; // no need to do anything
       `INST_OP_TYPE_S: begin
-        regs_addr1 <= rs1;
-        regs_addr2 <= rs2;
-        mem_write_en <= `true;
+        regs_addr1 = rs1;
+        operand1 = regs_data1;
+        operand2 = $signed(imm_s);
       end
       `INST_OP_TYPE_U_LUI,
       `INST_OP_TYPE_U_AUIPC: begin
-        regs_write_en <= `true;
-        regs_write_addr <= rd;
+        operand1 = inst_addr;
+        operand2 = {imm_u, {12{1'b0}}};
       end
       `INST_OP_TYPE_B: begin
-        regs_addr1 <= rs1;
-        regs_addr2 <= rs2;
+        regs_addr1 = rs1;
+        regs_addr2 = rs2;
+        operand1 = regs_data1;
+        operand2 = regs_data2;
+        // TODO: pause
       end
       `INST_OP_TYPE_J_JAL: begin
-        regs_write_addr <= rd;
+        operand1 = inst_addr;
+        operand2 = $signed({imm_j, 1'b0});
+        // TODO: pause
       end
     endcase
   end
